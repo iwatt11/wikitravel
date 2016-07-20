bb9dcb7537cf1d8f91e60a7f3d78f3710fd2826d
IAN WATT <ianwwatt@gmail.com>
2016-07-20 21:37:43 +0100

    Default to Town, State stuff if no geolocation. WIP.

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import location_model as lm
 import sys
-
+import json
+import os
 
 def main():
     coords = [34.057348, -118.273674]
     lat = coords[0]
     lng = coords[1]
     instance = lm.GetWikiData(lat, lng)
+    print instance.json_response
     #text_dict = wiki_work(namespace)
-    #sections = []
-    #for cat, desc in text_dict.iteritems():
-    #    sections.append(cat)
-    #print text_dict
-    #text_dict_json = json.dumps(text_dict)
-    #print text_dict_json
-    #summary = text_dict['Summary']
-    #history = text_dict['Notable people'].replace('(', '.').replace(')', '.')
-    #system('say %s' % (summary))
-    #system('say %s' % (history))
+    sections = []
+    for cat, desc in instance.json_response.iteritems():
+       sections.append(cat)
+
+    #text_dict_json = json.dumps(instance.json_response)
+
+    summary = instance.json_response['Summary']
+    history = instance.json_response['Notable people'].replace('(', '.').replace(')', '.')
+    os.system('say %s' % (summary))
+    os.system('say %s' % (history))
 
 
 
